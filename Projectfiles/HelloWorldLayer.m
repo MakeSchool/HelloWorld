21d51aaee658011c0516c39662662e4d2113b3a3
Brian Chu <bc@brianchu.com>
2012-09-05 17:08:10 -0700

    Init

---
+++ Projectfiles/HelloWorldLayer.m
@@ -0,0 +1,86 @@
+/*
+ * Kobold2D™ --- http://www.kobold2d.org
+ *
+ * Copyright (c) 2010-2011 Steffen Itterheim. 
+ * Released under MIT License in Germany (LICENSE-Kobold2D.txt).
+ */
+
+#import "HelloWorldLayer.h"
+#import "SimpleAudioEngine.h"
+
+CCSprite *ship1;
+CCSprite *ship2;
+
+@interface HelloWorldLayer (PrivateMethods)
+@end
+
+@implementation HelloWorldLayer
+
+@synthesize helloWorldString, helloWorldFontName;
+@synthesize helloWorldFontSize;
+
+-(id) init
+{
+	if ((self = [super init]))
+	{
+        
+        //Initialize the ship sprite with a specific file, the ship image
+        ship1 = [CCSprite spriteWithFile: @"ship.png"];
+        
+        //Set the ship's position. (0,0) is at the bottom left
+        ship1.position = ccp( 50, 200 );
+        
+        //Initialize the other sprite
+        ship2 = [CCSprite spriteWithFile: @"ship.png"];
+        
+        //Set the other ship's position
+        ship2.position = ccp( 100, 50 );
+        
+        //Add the ships to the game
+        [self addChild:ship1];
+        [self addChild:ship2];
+        
+        // schedule a repeating callback on every frame
+        [self scheduleUpdate];
+    }
+
+	return self;
+}
+
+//this method gets called every frame thanks to the [self scheduleUpdate]; line you just pasted in
+- (void) update:(ccTime)dt
+
+{
+    
+    //move the ship only in the x direction by a fixed amount every frame
+    ship1.position = ccp( ship1.position.x + 100*dt, ship1.position.y );
+    
+    if (ship1.position.x > 480+32)
+        
+    {
+        
+        //if the ship reaches the edge of the screen, loop around
+        ship1.position = ccp( -32, ship1.position.y );
+        
+    }
+    
+    //Create an instance called input of Kobold2D's built-in super easy to use touch processor
+    KKInput* input = [KKInput sharedInput];
+    
+    
+    //Create a point, pos, by asking input, our touch processor, where there has been a touch
+    CGPoint pos = [input locationOfAnyTouchInPhase:KKTouchPhaseAny];
+    
+    //input will return 0,0 if there has been no touch. We check for that, and move the ship if there was a touch!
+    if (pos.x != 0 && pos.y != 0)
+        
+    {
+        
+        //this line tells the ship to take 1 second to move to where you tapped
+        [ship2 runAction: [CCMoveTo actionWithDuration:1 position:pos]];
+        
+    }
+    
+}
+
+@end
